459bf621436b49a5a494ce9b355e44815a8302bf
Lokusok <lokusokdrok@gmail.com>
2023-11-19 22:56:42 +0300

    fix margin on main

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,6 @@ type Props = {
   children: React.ReactNode;
 };
 
-export default function layout({ children }: Props) {
-  return (
-    <html>
-      <body>{children}</body>
-    </html>
-  );
+export default function RootLayout({ children }: Props) {
+  return children;
 }
